8211efc805dfa301f7745f3161b0322cd3acf8d9
Sachin79 <amanraw2@gmail.com>
2022-08-21 11:36:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5503878652:AAFPZWeZ0q_PAPub8JO3nOQxVCi5lmaGl3A")
+    APP_ID = int(os.environ.get("APP_ID", 18028033))
+    API_HASH = os.environ.get("API_HASH","0501ff3a1a276f92534e335f9a76b6e6")
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
